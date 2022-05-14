abb57affbe39a8b001223335ed68db34288572d2
Oculto15 <93210080+Oculto15@users.noreply.github.com>
2022-05-14 11:55:54 -0600

    .

--- tasks/week04/scripts/task4.js
+++ tasks/week04/scripts/task4.js
@@ -20,25 +20,85 @@
 
 // Step 9: Add additional objects with the same properties for each place you've lived
 
+let myInfo = {
+    name: 'Ignacio Villar',
+    photo: 'images/photo.png',
+    favoriteFoods: [
+        'Sushi',
+        'Pizza',
+        'Burrito',
+        'Salad',
+        'Chicken',
+        'Steak'
+    ],
+    hobbies: [
+        'Play Soccer',
+        'Watch Movies',
+        'Cookiing'
+    ],
+    placesLived: [
+        {
+            place: 'Tucuman, Argentina',
+            length: '11 years'
+        },
+        {
+            place: 'Santiago, Chile',
+            length: '12 years'
+        },
+        {
+            place: 'Monevideo, Uruguay',
+            length: '2 year'
+        },
+        {
+            place: 'Rexburg, Idaho',
+            length: '1 year'
+        }
+    ]
+};
+
+
 
 /* OUTPUT */
 
 // Step 1: Assign the value of the name property (of the object declared above) to the HTML <span> element with an ID of name
+document.querySelector('#name').textContent = myInfo.name;
 
 // Step 2: Assign the value of the photo property as the src attribute of the HTML <img> element with an ID of photo
+document.querySelector('#photo').setAttribute('src', myInfo.photo);
 
 // Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
+document.querySelector('#photo').setAttribute('alt', myInfo.photo);
 
-// Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
+/// Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
+myInfo.favoriteFoods.forEach((food) => {
+    let favoriteFoodElem = document.createElement('li');
+    favoriteFoodElem.textContent = food;
 
 // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
+    document.querySelector('#favorite-foods').appendChild(favoriteFoodElem);
+});
 
 // Step 6: Repeat Step 4 for each hobby in the hobbies property
+myInfo.hobbies.forEach((hobby) => {
+    let hobbyElem = document.createElement('li');
+    hobbyElem.textContent = hobby;
 
 // Step 7: Repeat Step 5 using the HTML <ul> element with an ID of hobbies
+    document.querySelector('#hobbies').appendChild(hobbyElem);
+});
+
 
 // Step 8: For each object in the <em>placesLived</em> property:
 // - Create an HTML <dt> element and put its place property in the <dt> element
 // - Create an HTML <dd> element and put its length property in the <dd> element
-
-// Step 9: Append the HTML <dt> and <dd> elements created above to the HTML <dl> element with an ID of places-lived
+myInfo.placesLived.forEach((placeLived) => {
+    let placesLivedPlace = document.createElement('dt');
+    placesLivedPlace.textContent = placeLived.place;
+    
+    let placesLivedLength = document.createElement('dd');
+    placesLivedLength.textContent = placeLived.length;
+    
+    // Step 9: Append the HTML <dt> and <dd> elements created above to the HTML <dl> element with an ID of places-lived
+    document.querySelector('#places-lived').appendChild(placesLivedPlace);
+    document.querySelector('#places-lived').appendChild(placesLivedLength);
+})
